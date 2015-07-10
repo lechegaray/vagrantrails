fe46f151c1db7d67e3f00c7d226e2bca7df19561
Luis Echegaray <mrluisechegaray@gmail.com>
2015-07-10 11:09:56 -0400

    changed readme, added more commands to provision

--- script.sh
+++ script.sh
@@ -7,4 +7,8 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password_again passwo
 sudo apt-get -y update
 sudo apt-get -y install -y mysql-server
 
+cd alma
+sudo npm install -g bower
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
 echo "cd /vagrant" >> /home/vagrant/.bashrc
